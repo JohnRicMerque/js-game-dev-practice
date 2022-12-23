5fc0ca154294b53030429904bb05f83e5bf38e31
John Ric Merque <johnricmer@gmail.com>
2022-12-23 21:46:54 +0800

    animates all layers by array and forEach method

--- parallax-backgrounds/script.js
+++ parallax-backgrounds/script.js
@@ -6,7 +6,7 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 
-let gameSpeed = 15 
+let gameSpeed = 5 
 
 // importing and sourcing images
 const backgroundLayer1 = new Image();
@@ -64,6 +64,8 @@ class Layer {
         this.speed = gameSpeed * speedModifier
     }
     update() {
+        this.speed = gameSpeed * this.speedModifier
+        
         // reset position if reached -2400, then fix gaps by adding this.x - this.speed
         if (this.x <= -this.width){
             this.x = this.width + this.x2 - this.speed
@@ -71,6 +73,7 @@ class Layer {
         if (this.x2 <= -this.width){
             this.x2 = this.width + this.x - this.speed 
         }
+
         // moves image to the left by decrementing x and x2 with the game speed
         this.x = Math.floor(this.x - this.speed); 
         this.x2 = Math.floor(this.x2 - this.speed);    
@@ -81,13 +84,22 @@ class Layer {
     }
 }
 
-const layer4 = new Layer(backgroundLayer4, 0.5)
+const layer1 = new Layer(backgroundLayer1, 0.2)
+const layer2 = new Layer(backgroundLayer2, 0.4)
+const layer3 = new Layer(backgroundLayer3, 0.6)
+const layer4 = new Layer(backgroundLayer4, 0.8)
+const layer5 = new Layer(backgroundLayer5, 1)
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5]
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
 
-    layer4.update();
-    layer4.draw();
+    // for each array element, which is here called object, call this callback function that uses update and draw method of the layer instance
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    })
 
     requestAnimationFrame(animate);
 };
