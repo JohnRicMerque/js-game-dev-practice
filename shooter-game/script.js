bbe8a8439305a686fddd80d87422b9d53f592794
John Ric Merque <johnricmer@gmail.com>
2022-12-29 18:25:55 +0800

    added explosions sound effects and sprite animation

--- shooter-game/script.js
+++ shooter-game/script.js
@@ -16,7 +16,7 @@ let timeToNextRaven = 0; // accumulate ms values between frames until it reaches
 let ravenInterval = 500; // value in miliseconds, everytime it accumulates enough to reach 500ms it triggers next raven and resets 0
 let lastTime = 0; // holds value of timestamp
 
-let ravens = [];
+let ravens = []; // houses all raven objects
 
 class Raven {
     constructor(){
@@ -38,7 +38,6 @@ class Raven {
         this.flapInterval = Math.random() * 50 + 50; // time (ms) for each flap of raven wings
         this.randomColors = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)] // create random colors rgb
         this.color = 'rgb(' + this.randomColors[0] + ',' + this.randomColors[1] + ',' + this.randomColors[2] + ')';
-
 
     }
     update(deltaTime){
@@ -75,6 +74,39 @@ class Raven {
     }
 }
 
+let explosions = []; // houses all explosion objects
+
+class Explosions {
+    constructor(x, y, size){
+        this.image = new Image();
+        this.image.src = './images/boom.png'
+        this.spriteWidth = 200;
+        this.spriteHeight = 179;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.frame = 0;
+        this.sound = new Audio();
+        this.sound.src = './images/boom.wav'
+        this.timeSinceLastFrame = 0;
+        this.frameInterval = 200;
+        this.markedForDeletion = false;
+
+    }
+    update(deltaTime){
+        if (this.frame === 0) this.sound.play(); // play audio every start of each explosion sprite animation
+        this.timeSinceLastFrame += deltaTime;
+        if (this.timeSinceLastFrame > this.frameInterval){ // increase frame index if time since last frame reaches the given interval
+            this.frame++;
+            this.timeSinceLastFrame = 0;
+            if (this.frame > 5 ) this.markedForDeletion = true;
+        }
+    }
+    draw(){
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.size, this.size)
+    } 
+}
+
 function drawScore(){
     ctx.fillStyle = 'white';
     ctx.fillText('Score: ' + score, 50, 75)
@@ -88,9 +120,10 @@ window.addEventListener('click', function(e){
 
     ravens.forEach(object => {
         // comparing color data in constructor with the one clicked, if same color delete the raven and add score
-        if (object.randomColors[0] === pc[0] && object.randomColors[1] === pc[1] && object.randomColors[2] === pc[2]) {
+        if (object.randomColors[0] === pc[0] && object.randomColors[1] === pc[1] && object.randomColors[2] === pc[2]) { // for each object check which raven has hitbox color same as the clicked and mark it for deletion
             object.markedForDeletion = true;
             score++;
+            explosions.push(new Explosions(object.x, object.y, object.width))
         }
     })
 })
@@ -116,11 +149,12 @@ function animate(timestamp){ // timestamp is a built in argument that calculates
 
     drawScore();
 
-    [...ravens].forEach(object => object.update(deltaTime)); // spread operator is used to have multiple elements be run simultaneously
-    [...ravens].forEach(object => object.draw())
+    [...ravens, ...explosions].forEach(object => object.update(deltaTime)); // spread operator is used to have multiple elements be run simultaneously
+    [...ravens, ...explosions].forEach(object => object.draw())
 
     // this deletes ravens outside canvas
     ravens = ravens.filter(object => !object.markedForDeletion) // reassign ravens array with the filtered array that returns only objects that is not marked for deletion
+    explosions = explosions.filter(object => !object.markedForDeletion)
 
     requestAnimationFrame(animate)
 }
