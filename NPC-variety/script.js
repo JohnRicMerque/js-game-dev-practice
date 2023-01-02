ebefd5e2776fe68a6df7621382226feae7cee64e
John Ric Merque <johnricmer@gmail.com>
2023-01-03 07:12:13 +0800

    randomized enemy types

--- NPC-variety/script.js
+++ NPC-variety/script.js
@@ -11,8 +11,9 @@ document.addEventListener('DOMContentLoaded', function(){ // run all code when a
             this.width = width;
             this.height = height;
             this.enemies = [];
-            this.enemyInterval = 1000; // time (ms) interval before adding new enemy
-            this.enemyTimer = 0; // timer resets to 0 when reaches 400 
+            this.enemyInterval = 200; // time (ms) interval before adding new enemy
+            this.enemyTimer = 0; // timer resets to 0 when reaches 400
+            this.enemyTypes = ['worm', 'ghost']; 
         }
         update(deltaTime){
             this.enemies = this.enemies.filter(object => !object.markedForDeletion) // deletes enemy elements passing out the canvas by reassigning values of the array to house only objects that are not marked for deletion (false value)
@@ -23,14 +24,24 @@ document.addEventListener('DOMContentLoaded', function(){ // run all code when a
                 this.enemyTimer += deltaTime;
             }
 
-            this.enemies.forEach(object => object.update());
+            this.enemies.forEach(object => object.update(deltaTime));
         }
         draw(){
             this.enemies.forEach(object => object.draw(this.ctx))
 
         }
         #addNewEnemy(){ // if method starts with hash it is a private class method and can only be called within the class (here, Game class)
-            this.enemies.push(new Enemy(this)); // this inside enemy refers to the Game class
+            const randomEnemy = this.enemyTypes[Math.floor(Math.random() * this.enemyTypes.length)] // to randomize enemy types -  select randomly from enemyTypes Array by randomizing index from 0 to length of array and estimating floor to remove decimals 
+
+            if (randomEnemy == 'worm') {
+                this.enemies.push(new Worm(this)); // this inside enemy refers to the Game class
+            } else if (randomEnemy == 'ghost'){
+                this.enemies.push(new Ghost(this));
+            }
+
+            this.enemies.sort(function(a,b){ // so that enemy elements with higher vertical value (y) are on top of those with lower to have that 2d space effect
+                return a.y - b.y; 
+            })
 
         }
     }
@@ -38,19 +49,45 @@ document.addEventListener('DOMContentLoaded', function(){ // run all code when a
     class Enemy {
         constructor(game) {
             this.game = game;
-            this.x = game.width;
-            this.y = Math.random() * this.game.height;
-            this.width = 100;
-            this.height = 100;
             this.markedForDeletion = false
         }
-        update(){
-            this.x--;
+        update(deltaTime){
+            this.x -= this.speed * deltaTime; // we use delta time here we multiply but it will make sure same pacing is done to enemy objects
             if (this.x < 0 - this.width) this.markedForDeletion = true;
         }
         draw(ctx){
-            ctx.fillRect(this.x, this.y, this.width, this.height)
+            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
         }
+    }
+
+    class Worm extends Enemy{ // subclassing, worm inherits from parent class Enemy
+        constructor(game){
+            super(game) // takes all constructor properties from the parent class (Game
+            this.spriteWidth = 229;
+            this.spriteHeight = 171;
+            this.width = this.spriteWidth/2;
+            this.height = this.spriteHeight/2;
+            this.x = game.width;
+            this.y = Math.random() * this.game.height;
+            this.image = worm; // this syntax utilizes the images in the html DOM, by calling its id no need for selector
+            this.speed = Math.random() * 0.1 + 0.1;
+        }
+
+    }
+
+    class Ghost extends Enemy{ // subclassing, worm inherits from parent class Enemy
+        constructor(game){
+            super(game) // takes all constructor properties from the parent class (Game
+            this.spriteWidth = 261;
+            this.spriteHeight = 209;
+            this.width = this.spriteWidth/2;
+            this.height = this.spriteHeight/2;
+            this.x = game.width;
+            this.y = Math.random() * this.game.height;
+            this.image = ghost; // this syntax utilizes the images in the html DOM, by calling its id no need for selector
+            this.speed = Math.random() * 0.3 + 0.1;
+        }
+
     }
 
     const game = new Game(ctx, canvas.width, canvas.height);
@@ -72,3 +109,5 @@ document.addEventListener('DOMContentLoaded', function(){ // run all code when a
     
     animate(0);
 });
+
+// an improved code structure that avoids global variables code repitition, uses deltaTime, private methods, class inheritance or sub classing
