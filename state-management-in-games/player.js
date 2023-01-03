9b1364dfe8e96c420e5d4b3341c1868f056bd6ca
John Ric Merque <johnricmer@gmail.com>
2023-01-03 21:23:43 +0800

    made the player change sprite frame y when listening to input handler

--- state-management-in-games/player.js
+++ state-management-in-games/player.js
@@ -1,8 +1,10 @@
+import { StandingLeft, StandingRight } from "./state.js";
+
 export default class Player {
     constructor(gameWidth, gameHeight){
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
-        this.states = [];
+        this.states = [new StandingLeft(this), new StandingRight(this)];
         this.currentState = this.states[0];
         this.image = document.getElementById('dogImage')
         this.width = 200;
@@ -14,5 +16,12 @@ export default class Player {
     }
     draw(context){
         context.drawImage(this.image, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+    update(input){ // argument here is string last key like "PRESSED ArrowLeft"
+        this.currentState.handleInput(input) // this.currentState here is a state instance like new StandingLeft(this) handles input that will determine set State
+    }
+    setState(state){ // state parameter here will be an index
+        this.currentState = this.states[state] // select current state object based on the index in states array
+        this.currentState.enter(); // changes the frameY which is essentially which sprite animation is used
     }
 }
--- state-management-in-games/script.js
+++ state-management-in-games/script.js
@@ -1,22 +1,24 @@
 import Player from './player.js';
 import InputHandler from './input.js';
+import { drawStatusText } from './utils.js';  // non default export note {}
 
 window.addEventListener('load', function(){
     const loading = this.document.getElementById('loading');
     loading.style.display = 'none'; // after all elements are loaded, remove loading h1 tag
 
-    const canvas = document.getElementById('canvas1');
+    const canvas = document.getElementById('canvas1'); // canvas setup
     const ctx = canvas.getContext('2d')
     canvas.width = this.window.innerWidth;
     canvas.height = window.innerHeight;
 
-    const player = new Player(canvas.width, canvas.height);
-    player.draw(ctx)
-
+    const player = new Player(canvas.width, canvas.height); // class instantiate
     const input = new InputHandler();
 
-    function animate(){
-        console.log(input.lastKey)
+    function animate(){ // animation loop
+        ctx.clearRect(0,0,canvas.width,canvas.height); // clear canvas every frame
+        player.update(input.lastKey) // argument here is string last key like "PRESSED ArrowLeft"
+        player.draw(ctx)
+        drawStatusText(ctx, input, player);
         requestAnimationFrame(animate)
     }
     animate()
