13604680740999a32e079b1474b62dddb5dcddc8
John Ric Merque <johnricmer@gmail.com>
2023-01-04 16:01:17 +0800

    input handler can now control player

--- side-scroller-game/script.js
+++ side-scroller-game/script.js
@@ -41,15 +41,47 @@ window.addEventListener('load', function(){ // waits for all assets to load befo
             this.image = document.getElementById('playerImage');
             this.frameX = 0;
             this.frameY = 0;
-            this.speed = 0;
+            this.speed = 0; // horizontal speed
+            this.vy = 0; // vertical speeed
+            this.weight = 1;
         }
         draw(context){
             context.fillStyle = 'white';
             context.fillRect(this.x, this.y, this.width, this.height);
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height,  this.x, this.y, this.width, this.height)
         }
-        update(){
-            this.x += this.speed; // horizontal motion
+        update(input){
+            // horizontal motion
+            this.x += this.speed;
+            if (input.keys.indexOf('ArrowRight') > -1){ // index of here checks if a key is in the array
+                this.speed = 5;
+            } else if (input.keys.indexOf('ArrowLeft') > -1){
+                this.speed = -5;
+            } else if ((input.keys.indexOf('ArrowUp') > -1) && this.onGround()){ // jump only when player is on ground
+                this.vy -= 32;
+            } else {
+                this.speed = 0;
+            }
+            
+            // horizontal boundaries
+            if (this.x < 0) this.x = 0;
+            else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width
+
+            // vertical motion
+            this.y += this.vy;
+            if (!this.onGround()){
+                this.vy += this.weight;
+            } else {
+                this.vy = 0;
+            }
+
+            // vertical boundaries
+            if (this.y >= this.gameHeight - this.height){
+                this.y = this.gameHeight - this.height
+            }
+        }
+        onGround(){
+            return this.y >= this.gameHeight - this.height;
         }
     }
 
@@ -75,7 +107,7 @@ window.addEventListener('load', function(){ // waits for all assets to load befo
     function animate(){
         ctx.clearRect(0,0,canvas.width,canvas.height)
         player.draw(ctx)
-        player.update()
+        player.update(input)
         requestAnimationFrame(animate)
     }
 
