db915f6a1c6ace90632a28795ab7338f8ce07993
John Ric Merque <johnricmer@gmail.com>
2023-01-04 17:09:42 +0800

    animated sprite relative to delta time

--- side-scroller-game/script.js
+++ side-scroller-game/script.js
@@ -41,7 +41,11 @@ window.addEventListener('load', function(){ // waits for all assets to load befo
             this.y = this.gameHeight - this.height;
             this.image = document.getElementById('playerImage');
             this.frameX = 0;
+            this.maxFrame = 8;
             this.frameY = 0;
+            this.fps = 20; // how fast we swap between indiv animation frames in sprite sheet
+            this.frameTimer = 0;
+            this.frameInterval = 1000/this.fps;
             this.speed = 0; // horizontal speed
             this.vy = 0; // vertical speeed
             this.weight = 1;
@@ -49,9 +53,20 @@ window.addEventListener('load', function(){ // waits for all assets to load befo
         draw(context){
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height,  this.x, this.y, this.width, this.height)
         }
-        update(input){
+        update(input, deltaTime){
+            // sprite animation
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX >= this.maxFrame) this.frameX = 0; 
+                else this.frameX++;
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime;
+            }
+
             // horizontal motion
             this.x += this.speed;
+
+            // controls
             if (input.keys.indexOf('ArrowRight') > -1){ // index of here checks if a key is in the array
                 this.speed = 5;
             } else if (input.keys.indexOf('ArrowLeft') > -1){
@@ -64,16 +79,17 @@ window.addEventListener('load', function(){ // waits for all assets to load befo
             
             // horizontal boundaries
             if (this.x < 0) this.x = 0;
-            else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width
+            else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
 
             // vertical motion
             this.y += this.vy;
             if (!this.onGround()){ // if player not on ground
                 this.vy += this.weight; // add vertical speed
-                this.frameY = 1 // changes sprite sheet 
+                this.maxFrame = 5;
+                this.frameY = 1; // changes sprite sheet 
             } else {
                 this.vy = 0;
-                this.frameY = 0 // reset sprite sheet and vertixal speed
+                this.frameY = 0; // reset sprite sheet and vertixal speed
             }
 
             // vertical boundaries
@@ -119,13 +135,24 @@ window.addEventListener('load', function(){ // waits for all assets to load befo
             this.x = gameWidth;
             this.y = gameHeight - this.height;
             this.frameX = 0;
+            this.maxFrame = 5;
+            this.fps = 20; // how fast we swap between indiv animation frames in sprite sheet
+            this.frameTimer = 0;
+            this.frameInterval = 1000/this.fps;
             this.speed = 8;
         }
         draw(context){
             context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
         }
-        update(){
-            this.x -= this.speed;
+        update(deltaTime){
+            if (this.frameTimer > this.frameInterval){
+                if (this.frameX >= this.maxFrame) this.frameX = 0;
+                else this.frameX++
+                this.frameTimer = 0;
+            } else {
+                this.frameTimer += deltaTime
+            } // animate spritesheet relative to deltaTime
+            this.x -= this.speed; // move to the left
         }
     }
 
@@ -138,7 +165,7 @@ window.addEventListener('load', function(){ // waits for all assets to load befo
         }
         enemies.forEach(enemy => {
             enemy.draw(ctx)
-            enemy.update()
+            enemy.update(deltaTime)
         })
     }
 
@@ -164,7 +191,7 @@ window.addEventListener('load', function(){ // waits for all assets to load befo
         background.draw(ctx)
         background.update()
         player.draw(ctx)
-        player.update(input)
+        player.update(input, deltaTime)
         handleEnemies(deltaTime)
         requestAnimationFrame(animate)
     }
